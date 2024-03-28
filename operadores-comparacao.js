3239c7744ad8fef54c5267decdadfd3b1a819ee4
Leonardo <leonardoasantos1@outlook.com>
2024-03-27 23:19:02 -0300

    first commit

---
+++ operadores-comparacao.js
@@ -0,0 +1,11 @@
+function comparacao (num1, num2) {
+    return num1 > num2;
+    }
+    
+    let resultado = comparacao (3, 2);
+    let resultado2 = comparacao (10, 5);
+    let resultado3 = comparacao (7, 7);
+    
+    console.log(resultado);
+    console.log(resultado2);
+    console.log(resultado3);
